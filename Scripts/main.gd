27a7936f9e931c82a4784e549efea5631fe0ae7c
Aashraya Belbase <sparkdark103@gmail.com>
2026-01-01 20:38:30 +0545

    Added ScoreLabel

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,9 +3,12 @@ extends Node2D
 @onready var pipe_scene = preload("res://Scenes/pipes.tscn")
 
 func _ready() -> void:
-	$Timer.start()
+	$PipeTimer.start()
 
 func _on_timer_timeout() -> void:
 	var pipe = pipe_scene.instantiate()
 	pipe.position = Vector2(600, 0)  # start just off the right side of the screen
 	add_child(pipe)
+
+func _process(delta: float) -> void:
+	$ScoreDisplay/ScoreLabel.text = str(ScoreManager.score)
